49efa435ac41c3fb0b0f722de4db6a56ece0b282
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-09-23 17:42:11 -0300

    StyledButton component

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text, ImageBackground } from 'react-native';
 
+import StyledButton from '../StyledButton/index.js';
+
 import styles from './styles.js';
 
 const CarItem = (props) => {
@@ -14,6 +16,22 @@ const CarItem = (props) => {
                 <Text style={styles.title}>Model S</Text>
                 <Text style={styles.subtitle}>Starting at $69,420</Text>
             </View>
+
+            <StyledButton
+                type={'primary'}
+                content={'Custom Order'}
+                onPress={() => {
+                    console.warn("Custom Order was pressed");
+                }}
+            />
+
+            <StyledButton
+                type={'secondary'}
+                content={'Existing Inventory'}
+                onPress={() => {
+                    console.warn("Existing Inventory was pressed");
+                }}
+            />
         </View>
     );
 };
---
+++ components/StyledButton/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 10,
+        width: '100%'
+    },
+    button: {
+        alignItems: 'center',
+        borderRadius: 20,
+        height: 40,
+        justifyContent: 'center'
+    },
+    text: {
+        fontSize: 12,
+        fontWeight: '500',
+        textTransform: "uppercase",
+    }
+});
+
+export default styles;
