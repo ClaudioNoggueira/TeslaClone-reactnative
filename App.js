fe31a231640a675b25a549bd4426503bee329e8a
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-09-24 17:35:17 -0300

    Header component

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
+import Header from './components/Header';
 import CarList from './components/CarList';
 
 export default function App() {
   return (
     <View style={styles.container}>
+
+      <Header />
 
       <CarList />
 
